5105f26fdf90056e90db3ce903920aa0108542d1
MissSiS <julius.bmw@gmx.de>
2022-12-23 02:14:27 +0100

    make background transparent; add remap for moving lines

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -1,4 +1,7 @@
-vim.cmd[[colorscheme nvcode]]
+vim.cmd([[colorscheme nvcode]])
+vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+vim.api.nvim_set_hl(0, "SignColumn", { bg = "none" })
 
 -- vim.cmd[[colorscheme tokyonight]]
 -- vim.g.tokyonight_style = "night"
