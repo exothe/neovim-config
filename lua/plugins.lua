0178309ebb54763c583f039d271288edc02f8fcb
MissSiS <julius.bmw@gmx.de>
2023-01-18 21:28:15 +0100

    exchange auto-pairs with nvim-autopairs

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -28,7 +28,7 @@ return require("packer").startup(function(use)
 	use("nvim-treesitter/nvim-treesitter")
 	use("christianchiarulli/nvcode-color-schemes.vim")
 
-	use("jiangmiao/auto-pairs")
+	use("windwp/nvim-autopairs")
 	use("tpope/vim-surround")
 
 	use({
