cf5c9f54054a3bd5c0ab0d76f1825fb1815c327f
MissSiS <julius.bmw@gmx.de>
2023-01-20 21:45:54 +0100

    split lsp and cmp config into 2 files

---
+++ after/plugin/nvim_cmp.lua
@@ -0,0 +1,74 @@
+local cmp = require("cmp")
+local lspkind = require("lspkind")
+local luasnip = require("luasnip")
+
+-- better autocompletion experience
+vim.o.completeopt = "menuone,noselect"
+
+cmp.setup({
+	-- Format the autocomplete menu
+	formatting = {
+		format = lspkind.cmp_format({
+			with_text = true,
+			menu = {
+				buffer = "[buf]",
+				nvim_lsp = "[LSP]",
+				luasnip = "[snip]",
+			},
+		}),
+	},
+	mapping = {
+		-- Use Tab and shift-Tab to navigate autocomplete menu
+		["<C-J>"] = function(fallback)
+			if cmp.visible() then
+				cmp.select_next_item()
+			elseif luasnip.expand_or_jumpable() then
+				luasnip.expand_or_jump()
+			else
+				fallback()
+			end
+		end,
+		["<C-K>"] = function(fallback)
+			if cmp.visible() then
+				cmp.select_prev_item()
+			elseif luasnip.jumpable(-1) then
+				luasnip.jump(-1)
+			else
+				fallback()
+			end
+		end,
+		["<CR>"] = cmp.mapping.confirm({
+			behavior = cmp.ConfirmBehavior.Replace,
+			select = true,
+		}),
+		["<C-Space>"] = cmp.mapping({
+			i = function()
+				if cmp.visible() then
+					cmp.abort()
+				else
+					cmp.complete()
+				end
+			end,
+			c = function()
+				if cmp.visible() then
+					cmp.close()
+				else
+					cmp.complete()
+				end
+			end,
+		}),
+		["<C-e>"] = cmp.mapping.abort(),
+	},
+	snippet = {
+		expand = function(args)
+			luasnip.lsp_expand(args.body)
+		end,
+	},
+	sources = {
+		{ name = "luasnip" },
+		{ name = "nvim_lsp" },
+	},
+	experimental = {
+		ghost_text = true,
+	},
+})
