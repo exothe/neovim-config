51b6777621ccb2fcdf48294c6eb450afd0c1d8e9
MissSis <julius.bmw@gmx.de>
2023-04-19 21:36:22 +0200

    change tabwidth to 4; add function for setting it; use spaces, not tabs

--- lua/basics.lua
+++ lua/basics.lua
@@ -1,16 +1,23 @@
 local o = vim.o
 local g = vim.g
 
+function Set_tabwidth(tabwidth)
+	o.shiftwidth = tabwidth
+	o.tabstop = tabwidth
+end
+
 o.number = true
 o.relativenumber = true
 o.splitbelow = true
 o.splitright = true
 o.clipboard = "unnamedplus"
 o.mouse = "a"
-o.tabstop = 2
-o.shiftwidth = 2
+o.tabstop = 4
+o.shiftwidth = 4
+o.expandtab = true
 o.hlsearch = false
 o.ignorecase = true
 o.smartcase = true
+o.textwidth = 120
 
 g.mapleader = " "
