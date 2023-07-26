85694038f3a044de73ec2fde397946ba642c896c
MissSis <julius.bmw@gmx.de>
2023-07-26 22:43:18 +0200

    add latex settings and snippets

--- after/plugin/luasnip.lua
+++ after/plugin/luasnip.lua
@@ -1,4 +1,6 @@
 local ls = require("luasnip")
+local extras = require("luasnip.extras")
+local fmt = require("luasnip.extras.fmt").fmt
 local s = ls.snippet
 local t = ls.text_node
 local i = ls.insert_node
@@ -6,6 +8,7 @@ local f = ls.function_node
 local c = ls.choice_node
 local d = ls.dynamic_node
 local r = ls.restore_node
+local rep = extras.rep
 
 -- typescript snippets
 ls.config.set_config({
@@ -42,4 +45,65 @@ ls.add_snippets("tex", {
 			"\\end{enumerate}",
 		}),
 	}),
+	s(
+		{ trig = "fig", dscr = "A figure environment with caption and label." },
+		fmt(
+			[[
+            \begin{figure}
+                \centering
+                \includegraphics[width=\textwidth]{<>}
+                \caption{<>}
+                \label{fig:<>}
+            \end{figure}
+          ]],
+			{
+				i(1),
+				i(2),
+				rep(1),
+			},
+			{
+				delimiters = "<>",
+			}
+		)
+	),
+	s(
+		{ trig = "\\align", dscr = "An align environment." },
+		fmt(
+			[[
+            \begin{align}
+                <>
+            \end{align}
+          ]],
+			{
+				i(1),
+			},
+			{
+				delimiters = "<>",
+			}
+		)
+	),
+	s(
+		{ trig = "table", dscr = "A table environment with caption and label." },
+		fmt(
+			[[
+            \begin{table}
+                \centering
+                \begin{tabular}{<>}
+                    <>
+                \end{tabular}
+                \caption{<>}
+                \label{table:<>}
+            \end{table}
+          ]],
+			{
+				i(1),
+				i(0),
+				i(2),
+				i(3),
+			},
+			{
+				delimiters = "<>",
+			}
+		)
+	),
 })
--- lua/language_specifics.lua
+++ lua/language_specifics.lua
@@ -2,5 +2,7 @@ vim.api.nvim_create_autocmd("FileType", {
 	pattern = { "tex" },
 	callback = function()
 		vim.keymap.set("i", "<C-j>", "<Esc>A<CR>\\item ", { noremap = true })
+		vim.o.textwidth = 160
+		vim.o.cc = "+1"
 	end,
 })
