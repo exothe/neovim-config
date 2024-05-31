ef2df183d1b152d9988ec9afd36ec18d74c5ccdb
MissSis <julius.bmw@gmx.de>
2024-05-31 19:55:22 +0200

    disable treesitter in large files

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -3,7 +3,8 @@ require("nvim-treesitter.configs").setup({
 	highlight = {
 		enable = true,
 		disable = function(lang, bufnr)
-			return lang == "latex"
+			return lang == "latex" or
+                vim.api.nvim_buf_line_count(bufnr) > 50000
 		end,
 	},
 	matchup = {
