49740418b78c4a7086aec2b9f89d9fd66cb74bff
MissSis <julius.bmw@gmx.de>
2025-03-02 11:48:59 +0100

    show hidden files in telescope

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -6,9 +6,15 @@ n("<leader>ff", "<cmd>lua require('telescope.builtin').find_files()<cr>")
 n("<leader>fg", "<cmd>lua require('telescope.builtin').live_grep()<cr>")
 n("<leader>fb", "<cmd>lua require('telescope.builtin').buffers()<cr>")
 n("<leader>fh", "<cmd>lua require('telescope.builtin').help_tags()<cr>")
+n("<leader>fr", "<cmd>lua require('telescope.builtin').resume()<cr>")
 
-require('telescope').setup{
+require("telescope").setup({
 	defaults = {
-		file_ignore_patterns = {'node_modules/.*'}
-	}
-}
+		file_ignore_patterns = { "node_modules/.*" },
+	},
+	pickers = {
+		find_files = {
+			hidden = true,
+		},
+	},
+})
