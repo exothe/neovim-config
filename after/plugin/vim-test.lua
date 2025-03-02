6d1781eff3d012b076b21b880d7d277d914e3723
MissSis <julius.bmw@gmx.de>
2025-03-02 11:26:01 +0100

    vim-test: always use vitest for js and ts

--- after/plugin/vim-test.lua
+++ after/plugin/vim-test.lua
@@ -8,5 +8,8 @@ n("<leader>ts", ":TestSuite<CR>")
 n("<leader>tl", ":TestLast<CR>")
 n("<leader>tv", ":TestVisit<CR>")
 
-vim.cmd[[let test#java#gradletest#options = '-i']]
-vim.cmd[[let test#strategy = "neovim"]]
+vim.cmd([[let test#java#gradletest#options = '-i']])
+vim.cmd([[let test#strategy = "neovim"]])
+
+-- use vitests for all javascript and typescript tests
+vim.cmd([[let g:test#javascript#runner = 'vitest']])
